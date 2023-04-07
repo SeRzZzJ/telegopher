7271782f15eb1dd8e735d52d11d940a2a546ab93
SeRzZzJ <jc10102016@gmail.com>
2023-04-07 09:36:44 +0500

    feat: added user type support

--- core/telegram/types.go
+++ core/telegram/types.go
@@ -25,17 +25,17 @@ type Update struct {
 }
 
 type User struct {
-	Id                          int
-	Is_bot                      bool
-	First_name                  string
-	Last_name                   string
-	Username                    string
-	Language_code               string
-	Is_premium                  bool
-	Added_to_attachment_menu    bool
-	Can_join_groups             bool
-	Can_read_all_group_messages bool
-	Supports_inline_queries     bool
+	Id                          int    `json:"id"`
+	Is_bot                      bool   `json:"is_bot"`
+	First_name                  string `json:"first_name"`
+	Last_name                   string `json:"last_name,omitempty"`
+	Username                    string `json:"usernmae,omitempty"`
+	Language_code               string `json:"language_code,omitempty"`
+	Is_premium                  bool   `json:"is_premium,omitempty"`
+	Added_to_attachment_menu    bool   `json:"added_to_attachment_menu,omitempty"`
+	Can_join_groups             bool   `json:"can_join_groups,omitempty"`
+	Can_read_all_group_messages bool   `json:"can_read_all_group_messages,omitempty"`
+	Supports_inline_queries     bool   `json:"supports_inline_queries,omitempty"`
 }
 
 type Message struct {
